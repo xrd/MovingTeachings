d71be4d7fbaca136ae553abf6519a676cb6fa4f7
Chris Dawson <chris@vivoh.com>
2012-10-06 23:36:18 -0700

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movingteachings::Application.config.secret_token = '6e51a4139484202eb3b2db699eb5bf0396f0dd48647493723abf3c2339704f7c780a6cd72bad72cbd730c946eb030a8ac0fbaf2c8e4e3c678eabe097b9130537'
